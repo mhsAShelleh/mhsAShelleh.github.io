c4dc4ec306bd8ce7321faf3f4e81077fee116e4c
mhsAShelleh <48356579+mhsAShelleh@users.noreply.github.com>
2019-04-10 08:43:39 -0400

    Add files via upload

---
+++ using mouse and keyboard/sketch.js
@@ -0,0 +1,71 @@
+let x=100
+
+function setup() {
+  createCanvas(800, 600);
+  background(100,100,200);
+
+}
+
+function draw() {
+
+
+
+
+  rect(mouseX, mouseY,x/5, x/5);
+  if (mouseIsPressed) {
+    // When the mouse button is pressed
+    // change the colour randomly
+    fill(random(255), random(255), random(255));
+  }
+  if (mouseIsPressed) {
+    //when mouse button is heading the size changes
+    rect(mouseX,mouseY,x/2,x/2);
+    }
+
+    line(x/5, x/5,mouseX,mouseY);
+    if (mouseIsPressed) {
+      // When the mouse button is pressed
+      // direct the line
+}
+
+}
+
+
+
+
+
+
+
+function mouseDragged(){
+  rect(mouseX, mouseY,x/5, x/5);
+  if (mouseIsPressed) {
+    // When the mouse button is pressed
+    // change the colour randomly
+    fill(random(255), random(255), random(255));
+  }
+  if (mouseIsPressed) {
+    //when mouse button is heading the size changes
+    rect(mouseX,mouseY,x/2,x/2);
+    }
+
+}
+
+
+function keyPressed(){
+  if (keyIsPressed === true) {
+    background(100,100,200);
+  } else {
+   (0);
+  }
+  background(100,100,200);}
+
+function mouseDragged(){
+  line(x/5, x/5,mouseX,mouseY);
+  if (mouseIsPressed) {
+    // When the mouse button is pressed
+    // direct the line
+
+}
+
+
+}
