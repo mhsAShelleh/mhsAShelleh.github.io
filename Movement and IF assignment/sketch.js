6c495b4bdc823ad97a54d8fae2d8704507bc1751
mhsAShelleh <48356579+mhsAShelleh@users.noreply.github.com>
2019-05-08 09:20:13 -0400

    Update sketch.js

--- Movement and IF assignment/sketch.js
+++ Movement and IF assignment/sketch.js
@@ -5,8 +5,8 @@ let y=100;
 let xSpeed=10;
 let ySpeed=10;
 
-let size=100
-let xsize=100
+let size=100;
+let xsize=100;
 
 function setup() {
 // put setup code here
@@ -21,11 +21,10 @@ function draw() {
   //change the background
   background(66, 152, 244);
   //Draw the rectangle
-  fill(193,135,19);
   rect(x,y,xsize,xsize);
   //Move the rectangle
-  x+=xSpeed
-  y+=ySpeed
+  x+=xSpeed;
+  y+=ySpeed;
 
 
   //If reaches bottom change direction
@@ -33,27 +32,20 @@ function draw() {
     ySpeed=ySpeed*(-1);
   }
 
-  /*//If reaches top change direction
-  else if(y<=0){
-    ySpeed=ySpeed*(-1);
-  }*/
 
   //If reaches the rigth side change direction
   if (x+size>=width || x<=0){
-    xSpeed=xSpeed*(-1)
+    xSpeed=xSpeed*(-1);
   }
 
-  /*//If reaches left side change direction
-  else if (x<=0){
-    xSpeed=xSpeed*(-1)
-  }*/
-
+//When hits a side make the sqaure even larger
   if(y+size>=height || y<=0){
-    xsize+=size/5
+    fill(122);
+    xsize+=size/4;
   }
 
   if (x+size>=width || x<=0){
-    xsize+=size/5
+    xsize-=size/4;
+    fill(193,135,19);
   }
 }
-
