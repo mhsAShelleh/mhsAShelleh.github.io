44ddb8da2de9e7a898f90a8445a63e8750c2e4a1
mhsAShelleh <48356579+mhsAShelleh@users.noreply.github.com>
2019-05-08 08:47:35 -0400

    Update sketch.js

--- Snowman Assignment/sketch.js
+++ Snowman Assignment/sketch.js
@@ -1,3 +1,5 @@
+/*---------------------------Snowman Assignment-------------*/
+
 function setup() {
   // put setup code here
   createCanvas(640,480);
@@ -6,6 +8,7 @@ function setup() {
 
 function draw() {
   // Snowman,Abdol,March 7
+  
   //create head
   ellipse(207, 78, 60, 60);
   fill(0, 0, 0);
@@ -13,14 +16,16 @@ function draw() {
   ellipse(222,74,-13,13);
   ellipse(208,91,-23,13);
   fill(255, 255, 255);
+  
   //create body
   ellipse(210,150,86,81);
   ellipse(215,264,145,146);
+  
   //create arms
   line(66,92,169,155);
   line(358,93,253,152);
-  //create buttons
-
+  
+  //create buttons on chest 
   ellipse(210,133,-9,8);
   ellipse(210,152,-9,8);
   ellipse(210,172,-9,8);
